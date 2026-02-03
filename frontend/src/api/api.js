349b05b648a8e3ab1ee69e57c1794ebeff8411e1
Tariq Rasheed <tariq.official1712@gmail.com>
2026-02-03 20:05:26 +0500

    new changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:`${import.meta.env.VITE_BACKEND_BASEURL}/api`, 
+  baseURL:`${import.meta.env.VITE_BACKEND_BASEURL}api`, 
   timeout: 15000,
   // headers: {
   //   "Content-Type": "application/json",
